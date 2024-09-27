85f40be76c74b9ec9e9dda902d35066e02bdd52d
Velsty <117530498+Velsty@users.noreply.github.com>
2024-09-27 15:25:06 -0500

    Venta.js

--- models/Venta.js
+++ models/Venta.js
@@ -1,22 +1,16 @@
 const mongoose = require('mongoose');
 
-const productoServicioSchema = new mongoose.Schema({
-  producto_servicio_id: { type: String, required: true }, // Agregar validación si es necesario
-  nombre: { type: String, required: true },
-  precio: { type: Number, required: true, min: 0 }, // Asegurarse de que el precio sea positivo
-  cantidad: { type: Number, required: true, min: 1 } // Asegurarse de que la cantidad sea positiva
-});
-
 const ventaSchema = new mongoose.Schema({
-  cliente_id: { type: String, required: true }, // Podrías cambiar a ObjectId si tienes un modelo Cliente
-  fecha: { type: Date, required: true, default: Date.now }, // Establece fecha por defecto
-  total: { type: Number, required: true, min: 0 }, // Asegúrate de que el total sea positivo
-  estado: {
-    type: String,
-    enum: ['cancelada', 'completada'],
-    default: 'completada'
-  },
-  productos_servicios: [productoServicioSchema]
-}, { timestamps: true }); // Agrega timestamps
+    cliente_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    fecha: { type: Date, required: true },
+    total: { type: Number, required: true },
+    estado: { type: String, required: true },
+    productos_servicios: [{
+        producto_servicio_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+        cantidad: { type: Number, required: true },
+        precio: { type: Number, required: true },
+        nombre: { type: String, required: true }
+    }],
+});
 
 module.exports = mongoose.model('Venta', ventaSchema);
