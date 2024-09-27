b3ab1e94658b60a4952a22768840b5902cee49af
Velsty <mrgerito@gmail.com>
2024-09-26 22:49:50 -0500

    Instalar express-validator y solucionar errores

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      dbName: 'Moe_Bar',
-    });
-    console.log('Conectado a la base de datos');
-  } catch (error) {
-    console.error('Error al conectar a la base de datos:', error);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Conexión exitosa a MongoDB');
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
